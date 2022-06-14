7b76eaadae0216a9fd13546ee06ab576ec053bf4
Wisanu <wisanu.pho@cru.ac.th>
2022-06-14 09:45:09 +0700

    ternary

---
+++ ternary.js
@@ -0,0 +1,9 @@
+let age = 18
+let voteable = (age < 18) ? "Too young":"Old enough";
+console.log(voteable)
+//การใช้ == , === , !=,!==
+let x = (age == 18)? "way A":"way B";
+let y = (age == "18")? "way A":"way B";
+let z = (age === "18")? "way A":"way B";
+let a = (age != "18")? "way A":"way B";
+let b = (age !== "18")? "way A":"way B";
