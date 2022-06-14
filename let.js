66b3abb982be51ade54ec3c3c2660b52823ca0f1
Wisanu <wisanu.pho@cru.ac.th>
2022-06-14 09:23:58 +0700

    first commit

---
+++ let.js
@@ -0,0 +1,9 @@
+//การใช้let
+//let x = 10
+//console.log(x)
+let x = "john"
+if(x==="john"){
+      let x=5
+     console.log(x)
+}
+console.log(x)
---
+++ var.js
@@ -0,0 +1,13 @@
+//การใช้var
+var x = 10
+console.log(x)
+var x = "john"
+console.log (x)
+if(x==="john"){
+      var x=5
+     console.log(x)
+}
+console.log(x)
+y = "Peter"
+var y
+console.log(y)
