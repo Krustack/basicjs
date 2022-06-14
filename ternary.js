d9637d4f9146ad064eb2f9789aa48047f7781673
Wisanu <wisanu.pho@cru.ac.th>
2022-06-14 10:26:02 +0700

    update ternary

--- ternary.js
+++ ternary.js
@@ -7,3 +7,8 @@ let y = (age == "18")? "way A":"way B";
 let z = (age === "18")? "way A":"way B";
 let a = (age != "18")? "way A":"way B";
 let b = (age !== "18")? "way A":"way B";
+console.log(x)
+console.log(y)
+console.log(z)
+console.log(a)
+console.log(b)
